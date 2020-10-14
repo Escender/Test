7382656a2c5a2d6c08eb04920adfd1d08e4d6fd2
Escender.Sary <escender.sary@weigandt-consulting.com>
2020-10-14 22:42:55 +0300

    sd

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /**
  * @file        hello.c
- * @author      @$Author XXX $
- * @date        @$Date: 14 окт 2020 г. 22:33:39 $
+ * @author      @$Author: XXX $
+ * @date        @$Date: 14 окт 2020 г. 22:42:28 $
  * @version     @$Revision: d7d4076c476e152dad2e9b9a86affdbd92e82a35 $
  *
  * $Id: hello.c | Wed Oct 14 20:31:04 2020 +0300 | Escender.Sary  $
